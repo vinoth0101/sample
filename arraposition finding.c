eba8db01f1c9e3094d2ebc73165f61367c733c67
vinoth0101 <vinivinoth30@gmail.com>
2024-08-18 16:41:33 +0530

    update :remove unwanted line

--- arraposition finding.c
+++ arraposition finding.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : arraposition.c
- Author      : vinoth
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 #include <stdlib.h>
