de28e8a3f7aef2ba09bccdf54c0ec0d6f956e93a
vinoth0101 <vinivinoth30@gmail.com>
2024-08-18 16:40:13 +0530

    arraposition finding

---
+++ arraposition finding.c
@@ -0,0 +1,32 @@
+/*
+ ============================================================================
+ Name        : arraposition.c
+ Author      : vinoth
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int limit, values [100],i,SearchKey, flag=0;
+	setbuf(stdout,NULL);
+	printf("Enter a Limit");
+	scanf("%d",& limit);
+	printf("Enter Values");
+	for (i=0;i<limit; i++){
+	scanf("%d",&values[i]);
+	}
+	printf("Enter Search Key :");
+	scanf("%d",&SearchKey);
+	for (i=0;i<limit; i++){
+	if (SearchKey==values[i]) {
+		printf ("Value found at position %d",i+1);
+	break;
+	}
+
+	return EXIT_SUCCESS;
+}
